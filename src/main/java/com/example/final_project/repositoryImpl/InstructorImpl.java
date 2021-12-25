e79d34de137bb1f73bc61597a705e2dbd5c96aa4
nAzhre <erzh896@gmail.com>
2021-12-25 12:46:26 +0600

    entity

--- src/main/java/com/example/final_project/repositoryImpl/InstructorImpl.java
+++ src/main/java/com/example/final_project/repositoryImpl/InstructorImpl.java
@@ -14,6 +14,7 @@ import java.sql.SQLException;
 import java.sql.Statement;
 import java.util.List;
 
+@Transactional
 public class InstructorImpl implements InstructorRepository {
     @Inject
     EntityManager em;
